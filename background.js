26919d8cfe25b6adb075f11d91dcae925dcc0ddd
Derek Miranda <derek.miranda.1992@gmail.com>
2017-03-15 11:01:14 -0700

    Changed options page

--- background.js
+++ background.js
@@ -1,8 +1,5 @@
 const codewarsUrl = 'https://www.codewars.com/dashboard';
 
-if (!SiteBlocker.getWatchThisInstead()) {
-    SiteBlocker.setWatchThisInstead(chrome.extension.getURL("instead.html"));
-}
 chrome.tabs.onUpdated.addListener(function(tabId, changedInfo, tab) {
     blockPageUnlessCompletedTask(tab);
 });
@@ -13,18 +10,18 @@ function blockPageUnlessCompletedTask (tab) {
     chrome.storage.local.get('completedTask', (itemObj) => {
         const completedTask = itemObj['completedTask'];
         if (completedTask) {
+            return;
+        }
 
-        } else {
-            for (site in SiteBlocker.getBlockedSites()) {
-                if (tab.url.match(site)) {
-                    // store the wanted page in Session storage
-                    chrome.storage.local.set({
-                        'wantedPage', tab.url
-                    }, () => {
-                        // then redirect once user has completed task
-                        chrome.tabs.update(tab.id, {"url" : codewarsUrl}, function () {});
-                    });
-                }
+        for (site in SiteBlocker.getBlockedSites()) {
+            if (tab.url.match(site)) {
+                // store the wanted page in Session storage
+                chrome.storage.local.set({
+                    wantedPage: tab.url
+                }, () => {
+                    // then redirect once user has completed task
+                    chrome.tabs.update(tab.id, {"url" : codewarsUrl}, function () {});
+                });
             }
         }
     });
@@ -32,12 +29,12 @@ function blockPageUnlessCompletedTask (tab) {
 
 // if on wanted and then close --> completedTask = false
 // chrome.tabs.onRemoved.addListener(reset);
+// chrome.windows.onCreated.addListener(reset);
 chrome.windows.onRemoved.addListener(reset);
-chrome.windows.onCreated.addListener(reset);
 
-function reset () {
+function reset (cb) {
     chrome.storage.local.set({
-        'wantedPage': null,
-        'completedTask': false
-    }, () => {});  
+        wantedPage: null,
+        completedTask: false
+    }, cb);  
 }
